256eb4e1329cd0b1ae77c0e762b4ec5556490180
sumon <sumoncpi10@gmail.com>
2022-05-14 00:11:51 +0600

    env ok

--- src/firabase.init.js
+++ src/firabase.init.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCkjTm97AtHB6QhzS-SDminBB57i8ezn48",
-    authDomain: "xcn-warehouse.firebaseapp.com",
-    projectId: "xcn-warehouse",
-    storageBucket: "xcn-warehouse.appspot.com",
-    messagingSenderId: "710385020410",
-    appId: "1:710385020410:web:887e3662f96327b6234843",
-    measurementId: "G-5PW9BJQR7S"
+    apiKey: process.env.REACT_APP_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
+    measurementId: process.env.REACT_APP_measurementId,
 };
 
 // Initialize Firebase
