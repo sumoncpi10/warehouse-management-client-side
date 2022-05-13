672aff723a9bf3826f3990e82e05fa11407ef20b
sumon <sumoncpi10@gmail.com>
2022-05-14 01:25:00 +0600

    read

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # XCN Warehouse
 
-This project was bootstrapped with React && MongoDB [XCN Warehouse](https://github.com/facebook/create-react-app).
+This project was bootstrapped with React && MongoDB [Live Site XCN Warehouse](https://github.com/facebook/create-react-app).
+
+
+
+This is a warehouse website. Here an organization can do all their work including stock management, updates.
+
+<li>This is a Software for Warehouse</li>
--- src/firabase.init.js
+++ src/firabase.init.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -7,13 +7,21 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
-    measurementId: process.env.REACT_APP_measurementId,
+    // apiKey: process.env.REACT_APP_apiKey,
+    // authDomain: process.env.REACT_APP_authDomain,
+    // projectId: process.env.REACT_APP_projectId,
+    // storageBucket: process.env.REACT_APP_storageBucket,
+    // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    // appId: process.env.REACT_APP_appId,
+    // measurementId: process.env.REACT_APP_measurementId
+    apiKey: "AIzaSyCkjTm97AtHB6QhzS-SDminBB57i8ezn48",
+    authDomain: "xcn-warehouse.firebaseapp.com",
+    projectId: "xcn-warehouse",
+    storageBucket: "xcn-warehouse.appspot.com",
+    messagingSenderId: "710385020410",
+    appId: "1:710385020410:web:887e3662f96327b6234843",
+    measurementId: "G-5PW9BJQR7S"
+
 };
 
 // Initialize Firebase
