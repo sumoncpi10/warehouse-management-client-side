0aa1daa5f3a62872587d2e0355bb3f12d8c39643
sumon <sumoncpi10@gmail.com>
2022-05-14 23:11:14 +0600

    final final

--- src/Pages/Header/Header.js
+++ src/Pages/Header/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
     return (
         <div className='' >
 
-            <nav class="navbar navbar-expand-lg  navbar-dark bg-primary px-5">
+            <nav class="navbar navbar-expand-lg  navbar-dark bg-primary px-5 fixed-top">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
